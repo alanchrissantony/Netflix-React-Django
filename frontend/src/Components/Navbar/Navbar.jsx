d4c28788a8f81671dfa681e0f3f0505930a9abf2
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-03-02 19:17:21 +0530

    first commit

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Navbar.css";
+import "../../App.css";
+import Logo from "../../Images/netflix.png";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="flex text-gray-100 justify-between items-center py-3 px-4 fixed w-full z-50">
+        <div className="w-6/12">
+          <a href="/">
+            <img src={Logo} alt="logo" className="w-5/12 md:w-2/12" />
+          </a>
+        </div>
+
+        {/* <div>
+          <form action="" method="post">
+            <button className="bg-primary_red py-1 px-3 text-white md:text-lg rounded-md">
+              Logout
+            </button>
+          </form>
+        </div> */}
+
+        <div>
+          <a href="">
+            <button className="bg-primary_red py-1 px-3 text-white md:text-lg rounded-md">
+              Sign In
+            </button>
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
